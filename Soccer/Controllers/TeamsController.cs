e2af0303e71177c44c83b4a752a85acde292d00a
Art Weinstein <artur.weinstein@gmail.com>
2021-10-14 16:40:12 -0700

    finish styling and creating edit page for tournaments
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- Soccer/Controllers/TeamsController.cs
+++ Soccer/Controllers/TeamsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Soccer.Models;
@@ -6,62 +7,106 @@ using System.Linq;
 
 namespace Soccer.Controllers
 {
-  public class TournamentsController : Controller
+  public class TeamsController : Controller
   {
     private readonly SoccerContext _db;
 
-    public TournamentsController(SoccerContext db)
+    public TeamsController(SoccerContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Tournament> model = _db.Tournaments.ToList();
-      return View(model);
+      return View(_db.Teams.ToList());
     }
 
     public ActionResult Create()
     {
+      ViewBag.TournamentId = new SelectList(_db.Tournaments, "TournamentId", "TournamentName");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Tournament tournament)
+    public ActionResult Create(Team team, int TournamentId)
     {
-      _db.Tournaments.Add(tournament);
+      _db.Teams.Add(team);
+      _db.SaveChanges();
+      if (TournamentId != 0)
+      {
+        _db.TeamTournament.Add(new TeamTournament() {TournamentId = TournamentId, TeamId = team.TeamId});
+        // _db.StudentCourse.Add(new StudentCourse() { CourseId = CourseId, StudentId = student.StudentId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      var thisTournament = _db.Tournaments
-        .Include(tournament => tournament.JoinEntities)
-        .ThenInclude(join => join.Team)
-        .FirstOrDefault(tournament => tournament.TournamentId == id);
-      return View(thisTournament);
+      var thisTeam = _db.Teams
+      .Include(team => team.JoinEntities)
+      .ThenInclude(join => join.Tournament)
+      .FirstOrDefault(team => team.TeamId == id);
+      return View(thisTeam);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisTeam = _db.Teams.FirstOrDefault(team => team.TeamId == id);
+      ViewBag.TournamentId = new SelectList(_db.Tournaments, "TournamentId", "TournamentName");
+      return View(thisTeam);
     }
 
     [HttpPost]
-    public ActionResult Edit(Tournament tournament)
+    public ActionResult Edit(Team team, int TournamentId)
     {
-      _db.Entry(tournament).State = EntityState.Modified;
+      if (TournamentId != 0)
+      {
+        _db.TeamTournament.Add(new TeamTournament() { TournamentId = TournamentId, TeamId = team.TeamId });
+      }
+      _db.Entry(team).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddTournament(int id)
+    {
+      var thisTeam = _db.Teams.FirstOrDefault(team => team.TeamId == id);
+      ViewBag.TournamentId = new SelectList(_db.Tournaments, "TournamentId", "TournamentName");
+      return View(thisTeam);
+    }
+
+    [HttpPost]
+    public ActionResult AddTournament(Team team, int TournamentId)
+    {
+      if (TournamentId != 0)
+      {
+      _db.TeamTournament.Add(new TeamTournament() { TournamentId = TournamentId, TeamId = team.TeamId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
     {
-      var thisTournament = _db.Tournaments.FirstOrDefault(tournament => tournament.TournamentId == id);
-      return View(thisTournament);
+      var thisTeam = _db.Teams.FirstOrDefault(team => team.TeamId == id);
+      return View(thisTeam);
     }
-    [HttpPost, ActionName("Delete")]
 
+    [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisTournament = _db.Tournaments.FirstOrDefault(tournament => tournament.TournamentId == id);
-      _db.Tournaments.Remove(thisTournament);
+      var thisTeam = _db.Teams.FirstOrDefault(team => team.TeamId == id);
+      _db.Teams.Remove(thisTeam);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteTournament(int joinId)
+    {
+      var joinEntry = _db.TeamTournament.FirstOrDefault(entry => entry.TeamTournamentId == joinId);
+      _db.TeamTournament.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
---
+++ Soccer/Controllers/TournamentsController.cs
@@ -0,0 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Soccer.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soccer.Controllers
+{
+  public class TournamentsController : Controller
+  {
+    private readonly SoccerContext _db;
+
+    public TournamentsController(SoccerContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Tournament> model = _db.Tournaments.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Tournament tournament)
+    {
+      _db.Tournaments.Add(tournament);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTournament = _db.Tournaments
+        .Include(tournament => tournament.JoinEntities)
+        .ThenInclude(join => join.Team)
+        .FirstOrDefault(tournament => tournament.TournamentId == id);
+      return View(thisTournament);
+    }
+
+      public ActionResult Edit(int id)
+    {
+      var thisCourse = _db.Tournaments.FirstOrDefault(course => course.TournamentId == id);
+      return View(thisCourse);
+    }
+
+
+    [HttpPost]
+    public ActionResult Edit(Tournament tournament)
+    {
+      _db.Entry(tournament).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisTournament = _db.Tournaments.FirstOrDefault(tournament => tournament.TournamentId == id);
+      return View(thisTournament);
+    }
+    [HttpPost, ActionName("Delete")]
+
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTournament = _db.Tournaments.FirstOrDefault(tournament => tournament.TournamentId == id);
+      _db.Tournaments.Remove(thisTournament);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
